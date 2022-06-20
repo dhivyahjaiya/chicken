a311bb8b136973f65428931c510971858b20784f
dhivyahjaiya <107708354+dhivyahjaiya@users.noreply.github.com>
2022-06-20 18:00:20 +0800

    Add files via upload

---
+++ installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7491c2f8be998966aaea2d1817e1f6f37b164dc7',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7491c2f8be998966aaea2d1817e1f6f37b164dc7',
+            'dev_requirement' => false,
+        ),
+        'picqer/php-barcode-generator' => array(
+            'pretty_version' => 'v2.2.2',
+            'version' => '2.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../picqer/php-barcode-generator',
+            'aliases' => array(),
+            'reference' => '877fcb0e9279ac40646361a7d7a7c6d0e1639836',
+            'dev_requirement' => false,
+        ),
+    ),
+);
